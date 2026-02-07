16f7f40b4ec0950171cd7b911594109f11fade45
Lizqxel <fortnite102@yahoo.co.jp>
2026-02-07 19:52:02 +0900

    法人モードの営コメで、管理番号の直下に空行を入れるように

--- ui/main_window_functions.py
+++ ui/main_window_functions.py
@@ -542,7 +542,7 @@ ND：{nd}
                     management_id = ""
 
                 if management_id:
-                    formatted_text = f"{management_id}\n{formatted_text}"
+                    formatted_text = f"{management_id}\n\n{formatted_text}"
 
                 call_preference_text = format_data.get('call_preference', '').strip()
                 if call_preference_text:
